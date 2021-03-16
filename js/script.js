58d49b381635e3a507bcffc4b35378e9e3b5c05d
syuhada-ramadhan <suhadahada18@gmail.com>
2021-03-16 09:57:34 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// array hewan
+const hewan = ["Kambing", "Kelinci", "Kerbau", "Kucing", "Sapi"];
+
+// cetak elemen hasil slice dari array hewan
+console.log(hewan);
+console.log(hewan.slice(3));
+console.log(hewan.slice(1, 4));
